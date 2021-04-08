056dc82f3044933fb1b5a732878276e98155b656
CharlieYu99 <34127055+CharlieYu99@users.noreply.github.com>
2021-04-08 14:34:02 +0800

    player movement limitation added

--- Assets/Scripts/SpaceFighter.cs
+++ Assets/Scripts/SpaceFighter.cs
@@ -9,6 +9,11 @@ public class SpaceFighter : MonoBehaviour
     public static SpaceFighter instance;
     public float movespeed = 3.0f;
 
+    private float PositionX_min = -1.75f;
+    private float PositionX_max = 1.75f;
+    private float PositionY_max = 1.25f;
+    private float PositionY_min = -0.25f;
+
     private void Awake() {
         rigidBody = GetComponent<Rigidbody2D>();
         instance = this;
@@ -26,7 +31,17 @@ public class SpaceFighter : MonoBehaviour
 
     private void FixedUpdate() {
         rigidBody.velocity = new Vector2(Input.GetAxis("Horizontal")*movespeed, Input.GetAxis("Vertical")*movespeed);
+        // Vector2 thisPosition = new Vector2(rigidBody.position.x, rigidBody.position.y);
+        Vector2 thisPosition = rigidBody.transform.position;
+        if (!PointInsideRect(thisPosition, PositionX_min, PositionX_max, PositionY_max, PositionY_min)){
+            rigidBody.transform.position = new Vector2(Mathf.Clamp(thisPosition.x, PositionX_min, PositionX_max), Mathf.Clamp(thisPosition.y, PositionY_min, PositionY_max)); 
+        }
 
+        
+    }
+
+    private bool PointInsideRect(Vector2 point,float left, float right, float top, float buttom){
+        return (point.x >= left && point.x <= right && point.y >= buttom && point.y <= top);
     }
 
     void OnCollisionEnter2D(Collision2D collision){
